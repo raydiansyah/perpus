c44cafb13040afd30a591ef5cfae4ce15b4ddf93
raydiansyah <raydiansyah@gmail.com>
2026-09-01 15:26:56 +0700

    Add migration books table

---
+++ database/migrations/2026_09_01_080110_create_books_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table){
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')
+            ->constrained()
+            ->cascadeOnUpdate();
+            $table->string('title');
+            $table->string('penerbit')->nullable();
+            $table->unsignedInteger('stok')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
